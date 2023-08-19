5e38bea6477a405274d5b56964bcc77be6f732af
talia1711 <110748620+talia1711@users.noreply.github.com>
2023-08-19 17:37:32 +0100

    Hide Show the form

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+let searchForm = document.getElementById('search-form');
+let title = document.getElementById('title');
+let author = document.getElementById('author');
+
 function newBook(event) {
     console.log ('new book was clicked');
+    searchForm.style.display ='block';
+}
+
+function handleSubmit(event) {
+    console.log ('handleSubmit');
+    console.log ('title ', title.textContent);
+    fetch('').then((result)=>{
+        //loop on the result of books and add to the html
+
+
+    }).catch( (error)=>{} );
+}
+
+function cancel(event) {
+    //searchForm.style.display ='none';
 }
