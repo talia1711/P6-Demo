7cba07df349a2df75558b6ac1462fe0f7e944407
talia1711 <110748620+talia1711@users.noreply.github.com>
2023-08-22 21:01:32 +0100

    add remove bookmarks logic
    Add book data to the card div
    Add book to session storage array

--- results.js
+++ results.js
@@ -11,11 +11,26 @@ const url = new URL(
     window.location.href,
   );
 
-  const params = new URLSearchParams(window.location.search);
-  console.log(params); // Logs "?q=123"
-  console.log(params.get('title')); // Logs "?q=123"
-  console.log(params.get('author')); // Logs "?q=123"
+const params = new URLSearchParams(window.location.search);
+console.log(params); // Logs "?q=123"
+console.log(params.get('title')); // Logs "?q=123"
+console.log(params.get('author')); // Logs "?q=123"
 
+function addRemoveBookmark(event){
+    console.log('the button was clicked', event);
+    let parent = event.target.closest('.book-card');
+    let id = parent.getAttribute('id');
+    let title = parent.getAttribute('title');
+    let description = parent.getAttribute('description');
+    let image = parent.getAttribute('image');
+    let author = parent.getAttribute('author');
+    console.log('parents', event.target.closest('.book-card'));
+    let favs = sessionStorage.getItem("bookmark") || [];
+    favs.add({
+        id, title, description, image, author
+    });
+    favs.setItem('bookmark', favs);
+}
 
 fetch(`https://www.googleapis.com/books/v1/volumes?q=inauthor:${params.get('title')}&intitle=${params.get('author')}&key=AIzaSyAtMwLNihpdJaRUGogPtWvTk9v_fxomd9E`)
     .then((result)=>{
@@ -28,13 +43,21 @@ fetch(`https://www.googleapis.com/books/v1/volumes?q=inauthor:${params.get('titl
     for (let item of data.items){
         var bookElementDiv = document.createElement("div");
         bookElementDiv.className = 'book-card';
-        bookElementDiv.innerHTML = `<p><b>Title:</b> ${item.volumeInfo.title}</p>
+        bookElementDiv.setAttribute('id',item.id);
+        bookElementDiv.setAttribute('title',item.volumeInfo.title);
+        bookElementDiv.setAttribute('description',item.volumeInfo.description?.substring(0,50));
+        bookElementDiv.setAttribute('image',item.volumeInfo?.imageLinks?.thumbnail);
+        bookElementDiv.setAttribute('author',item.volumeInfo.authors);
+        bookElementDiv.innerHTML = `<div class = "bookmark">  
+                                        <i class="fa fa-bookmark" onclick="addRemoveBookmark(event)"></i>
+                                    </div>       
+                                    <p><b>Title:</b> ${item.volumeInfo.title}</p>
                                     <p><b>Id:</b> ${item.id}</p>
                                     <p><b>Author:</b> ${item.volumeInfo.authors}</p>
                                     <p>
-                                    <b>Description:</b>: ${item.volumeInfo.description.substring(0,50)}
+                                    <b>Description:</b>: ${item.volumeInfo.description?.substring(0,50)}
                                     </p>
-                                    <img src="${item.volumeInfo.imageLinks.thumbnail}" width="200px" height="200px" alt="" />`;
+                                    <img src="${item.volumeInfo?.imageLinks?.thumbnail}" width="200px" height="200px" alt="" />`;
 
         listResults.appendChild (bookElementDiv);
         console.log (item.volumeInfo.title);        
